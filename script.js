4a8d4d150cd936e5e259cad463b7d92e81e3d050
YCien <136769667+YCien@users.noreply.github.com>
2023-12-24 12:02:02 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const clickMe = document.getElementById('click-me');
+    const result = document.getElementById('result');
+    const backgroundMusic = new Howl({
+        src: ['background-music.mp3'],
+        loop: true,
+        volume: 0.5,
+        preload: true,
+        autoplay: false
+    });
+
+    clickMe.addEventListener('click', function () {
+        result.textContent = '无论做什么 开心最重要啦 圣诞快乐!';
+        backgroundMusic.play();
+    });
+});
