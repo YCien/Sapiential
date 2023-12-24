7bd49ec06ab131595af043c45614e3b8c7198cad
YCien <136769667+YCien@users.noreply.github.com>
2023-12-24 11:33:46 +0800

    Delete text.js

--- text.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const clickMe = document.getElementById('click-me');
-    const result = document.getElementById('result');
-
-    clickMe.addEventListener('click', function () {
-        result.textContent = 'You clicked me!';
-    });
-});
